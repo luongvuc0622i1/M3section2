04256846d7c4992a5ac77a6188b8ac25375987ac
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-09-15 14:40:04 +0700

    Quan Ly Ban Hang re

--- QuanLyBanHang.sql
+++ QuanLyBanHang.sql
@@ -1,1 +1,31 @@
-SELECT * FROM QuanLyBanHang.OrderDetail;
+CREATE DATABASE QuanLyBanHang;
+
+USE QuanLyBanHang;
+
+CREATE TABLE Customer (
+    cId VARCHAR(20) PRIMARY KEY,
+    cName VARCHAR(50),
+    cAge INT
+);
+
+CREATE TABLE Oder (
+    oId VARCHAR(20) PRIMARY KEY,
+    cId VARCHAR(20),
+    oDate DATETIME,
+    oTotalPrice INT,
+    FOREIGN KEY (cID) REFERENCES Customer(cID)
+);
+
+CREATE TABLE Product (
+    pId VARCHAR(20) PRIMARY KEY,
+    pName VARCHAR(50),
+    pPrice INT
+);
+
+CREATE TABLE OrderDetail (
+    oId VARCHAR(20) PRIMARY KEY,
+    pId VARCHAR(20),
+    odQTY INT,
+    FOREIGN KEY (oID) REFERENCES Oder(oID),
+    FOREIGN KEY (pID) REFERENCES Product(pID)
+);
